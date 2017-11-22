26a576b230855770b743349dca3b057b72f1c14d
kuwkuw <oleg.kushch85@gmail.com>
2017-11-22 13:41:37 +0200

    Added local-storage saving/loading data

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -1,38 +1,48 @@
 
 export default () => {
-    'use strict';
+  'use strict';
 
-    if (!Vue) {
-        console.log('Vue is "undefined"');
-        return;
-    }
+  if (!Vue) {
+    console.log('Vue is "undefined"');
+    return;
+  }
 
-    Vue.debug = true;
+  Vue.debug = true;
 
-    new Vue({
-        el: '#form-app',
-        data() {
-            return {
-                userInfo: {
-                    firstName: '',
-                    lastName: '',
-                    userEmail: '',
-                    userAge: '',
-                    userSkype: ''
-                },
-                users: []
-            };
+  new Vue({
+    el: '#form-app',
+    data() {
+      return {
+        userInfo: {
+          firstName: '',
+          lastName: '',
+          userEmail: '',
+          userAge: '',
+          userSkype: ''
         },
-        methods: {
-            addInfo() {
-                this.users.push(Object.assign({}, this.userInfo));
-                Object.keys(this.userInfo).forEach(key => {
-                    this.userInfo[key] = '';
-                });
-            }
-        },
-        created() {
-            console.log('Vue app is loaded');
+        users: []
+      };
+    },
+    methods: {
+      addInfo() {
+        this.users.push(Object.assign({}, this.userInfo));
+        this.saveUsersToLocalStorage();
+        Object.keys(this.userInfo).forEach(key => {
+          this.userInfo[key] = '';
+        });
+      },
+      saveUsersToLocalStorage() {
+        localStorage.setItem('users', JSON.stringify(this.users));
+      },
+      loadUsersFromLocalStorage() {
+        const users = JSON.parse(localStorage.getItem("users"));
+        if (users) {
+          this.users = users;
         }
-    });
+      }
+    },
+    created() {
+      this.loadUsersFromLocalStorage();
+    }
+  });
 };
