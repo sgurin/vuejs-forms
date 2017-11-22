28ad51f51a8f8dd3f53d4b335cc361a7407d6094
Oleg Kushch <okushch@justcoded.co>
2017-11-22 12:16:36 +0200

    Added user form and listing for users information

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -2,17 +2,34 @@
 export default () => {
     'use strict';
 
-    if(!Vue){
+    if (!Vue) {
         console.log('Vue is "undefined"');
         return;
     }
-    
+
     Vue.debug = true;
 
     new Vue({
         el: '#form-app',
         data() {
-            return {};
+            return {
+                userInfo: {
+                    firstName: '',
+                    lastName: '',
+                    userEmail: '',
+                    userAge: '',
+                    userSkype: ''
+                },
+                users: []
+            };
+        },
+        methods: {
+            addInfo() {
+                this.users.push(Object.assign({}, this.userInfo));
+                Object.keys(this.userInfo).forEach(key => {
+                    this.userInfo[key] = '';
+                });
+            }
         },
         created() {
             console.log('Vue app is loaded');
